3f008ad648e940a66880814ca2a90989c8677377
manikanta Routhu <manikantar129@gmail.com>
2026-04-18 18:39:47 +0530

    change type

--- 01-vpc/parameters.tf
+++ 01-vpc/parameters.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_parameter" "vpc_id" {
     name = "/${var.project_name}/${var.environment}/vpc_id"
-    type = "String"
+    type = "StringList"
     value = module.vpc.vpc_id
 }
 
