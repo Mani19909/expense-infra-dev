7281e6802e333e51fb2abc56c3ff2fd9051efaab
manikanta Routhu <manikantar129@gmail.com>
2026-04-23 10:47:49 +0530

    expense infra dev

--- 10-cdn/data.tf
+++ 10-cdn/data.tf
@@ -7,5 +7,5 @@ data "aws_cloudfront_cache_policy" "cache_disable" {
 }
 
 data "aws_ssm_parameter" "acm_certificate_arn" {
-    name = "/${var.project_name}/${var.environment}/acm_certicate_arn"
+    name = "/${var.project_name}/${var.environment}/acm_certificate_arn"
 }
